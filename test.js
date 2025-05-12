e4171f2f31a2ff34450eeb155afbdc3f1627c503
nianod <arnoldkk422@gmail.com>
2025-05-13 00:39:43 +0300

    initialized modules

--- test.js
+++ test.js
@@ -1,21 +1,25 @@
 const mainContainer = document.querySelector(".container");
-const miniContainer = document.querySelector("sub-container");
+const miniContainer = document.querySelector(".sub-container");
 const link = document.getElementById("link");
 const downloadBtn = document.getElementById("download") 
 let url = true
 
- 
-link.addEventListener("paste", function() {
-    if(!url) {
-        
+ //Demo before getting The main API
+link.addEventListener("paste", function(event) {
+    
+    const pastesData = (event.clipboardData || window.clipboardData).getData("text")
+       showarn = document.createElement("i")
+
+    if(!pastesData.startsWith("https://")) { //Meaning not starting with https
+        url = false;
+        showarn.textcontent = "invalid link";
+    } else {
+        url = true;
+        showarn.style.display = "none"
     }
-    console.log("pated");
+ miniContainer.appendChild(showarn)
     
 })
 
 
-//Debugging
-downloadBtn.addEventListener("click", () => {
-    alert("cliked")
-})
  
